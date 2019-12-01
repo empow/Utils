dbeed7b2f1b4ea7c788605fd5ff66488125970a8
Assaf <assaf.abulafia@gmail.com>
2019-12-01 22:26:37 +0200

    Update Inet4RangeMatcher.java

--- src/main/java/co/empow/networkutils/Inet4RangeMatcher.java
+++ src/main/java/co/empow/networkutils/Inet4RangeMatcher.java
@@ -3,6 +3,7 @@ package co.empow.networkutils;
 import com.google.common.collect.*;
 import com.google.common.net.InetAddresses;
 import org.apache.commons.net.util.SubnetUtils;
+import sun.nio.ch.Net;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
@@ -10,59 +11,53 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
-import static co.empow.networkutils.NetUtils.ip2Integer;
+import static co.empow.networkutils.NetUtils.ip2Long;
+
 
 public class Inet4RangeMatcher {
 
-    private final RangeSet<Integer> ranges;
+    private final RangeSet<Long> ranges;
 
-    private Inet4RangeMatcher(RangeSet<Integer> ranges) {
+    private Inet4RangeMatcher(RangeSet<Long> ranges) {
         this.ranges = ranges;
     }
 
     public boolean inRange(String ip) {
-        InetAddress inetAddress = InetAddresses.forString(ip);
-        return inRange(inetAddress);
-    }
-
-    public boolean inRange(InetAddress ip) {
-
-        int numericValue = InetAddresses.coerceToInteger(ip);
-
-        return ranges.contains(numericValue);
+        long numericValueUnsgined = ip2Long(ip);
+        return ranges.contains(numericValueUnsgined);
     }
 
     public static class Builder {
-        private final Collection<Range<Integer>> ranges = new ArrayList<>();
+        private final Collection<Range<Long>> ranges = new ArrayList<>();
 
         public Builder() {
         }
 
         public Inet4RangeMatcher build() {
-            RangeSet<Integer> rangeSet = ImmutableRangeSet.unionOf(ranges);
+            RangeSet<Long> rangeSet = ImmutableRangeSet.unionOf(ranges);
 
             rangeSet = compact(rangeSet);
 
             return new Inet4RangeMatcher(rangeSet);
         }
 
-        private static RangeSet<Integer> compact(RangeSet<Integer> rangeSet) {
-            ImmutableRangeSet.Builder<Integer> builder = ImmutableRangeSet.builder();
+        private static RangeSet<Long> compact(RangeSet<Long> rangeSet) {
+            ImmutableRangeSet.Builder<Long> builder = ImmutableRangeSet.builder();
 
-            Set<Range<Integer>> descendingRanges = rangeSet.asDescendingSetOfRanges();
+            Set<Range<Long>> descendingRanges = rangeSet.asDescendingSetOfRanges();
 
-            Iterator<Range<Integer>> it = descendingRanges.iterator();
+            Iterator<Range<Long>> it = descendingRanges.iterator();
 
-            Range<Integer> previousRange = null;
+            Range<Long> previousRange = null;
 
             if (it.hasNext()) {
                 previousRange = it.next();
             }
 
             while (it.hasNext()) {
-                Range<Integer> currentItem = it.next();
+                Range<Long> currentItem = it.next();
 
-                Range<Integer> gap = currentItem.gap(previousRange);
+                Range<Long> gap = currentItem.gap(previousRange);
 
                 if (currentItem.isConnected(previousRange) ||
                         gap.lowerEndpoint() + 1 == gap.upperEndpoint()) {
@@ -78,16 +73,16 @@ public class Inet4RangeMatcher {
                 builder.add(previousRange);
             }
 
-            ImmutableRangeSet<Integer> compactedRanges = builder.build();
+            ImmutableRangeSet<Long> compactedRanges = builder.build();
 
             return TreeRangeSet.create(compactedRanges);
         }
 
         public Builder addRange(String fromAddress, String toAddress) {
-            int lowAddress = ip2Integer(fromAddress);
-            int highAddress = ip2Integer(toAddress);
+            long lowAddress = ip2Long(fromAddress);
+            long highAddress = ip2Long(toAddress);
 
-            Range<Integer> range = Range.closed(lowAddress, highAddress);
+            Range<Long> range = Range.closed(lowAddress, highAddress);
             ranges.add(range);
 
             return this;

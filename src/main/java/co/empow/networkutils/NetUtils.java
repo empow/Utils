35ff1458ec114bbf04861b95b848b019e9306e58
assaf79 <assaf.abulafia@gmail.com>
2020-03-21 10:29:04 +0200

    pom changes

--- src/main/java/co/empow/networkutils/NetUtils.java
+++ src/main/java/co/empow/networkutils/NetUtils.java
@@ -3,8 +3,12 @@ package co.empow.networkutils;
 import com.google.common.net.InetAddresses;
 
 import java.net.InetAddress;
+import java.util.regex.Pattern;
 
 public class NetUtils {
+
+    private static final Pattern ipV4Pattern = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+
     public static long ip2Long(String ip) {
         InetAddress address = InetAddresses.forString(ip);
         int ipAsInt = InetAddresses.coerceToInteger(address);
@@ -14,5 +18,9 @@ public class NetUtils {
 
     private static long int2long(int i) {
         return i & 0xffffffffL;
+    }
+
+    public static boolean isIpV4(String ip) {
+        return ipV4Pattern.matcher(ip).matches();
     }
 }
